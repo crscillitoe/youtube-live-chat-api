e96279664eb88e7764b6bcc9517f00f31ce84ce9
crscillitoe <you@example.com>
2023-10-30 04:14:24 +0000

    Strip out mongo/socket code and unnecessary handlers for sending messages

--- src/youtubeMessages.js
+++ src/youtubeMessages.js
@@ -4,27 +4,9 @@ const axios = require('axios');
 require('dotenv').config();
 
 const {
-  GOOGLE_REFRESH_TOKEN: refresh_token,
-  GOOGLE_CLIENT_ID: client_id,
-  GOOGLE_CLIENT_SECRET: client_secret,
   YOUTUBE_CHANNEL_ID: channelId,
   GOOGLE_API_KEY: key,
 } = process.env;
-
-let tokenInfo = null;
-
-async function getAccessToken() {
-  const {
-    data,
-  } = await axios.post('https://oauth2.googleapis.com/token', {
-    client_id,
-    client_secret,
-    refresh_token,
-    grant_type: 'refresh_token',
-  });
-  data.expires_time = Date.now() + ((data.expires_in - 1) * 1000);
-  return data;
-}
 
 
 const liveChatUrl = 'https://www.googleapis.com/youtube/v3/liveChat/messages';
@@ -155,35 +137,7 @@ function listenMessages(liveChatId) {
   return emitter;
 }
 
-async function sendMessage(liveChatId, messageText) {
-  if (!tokenInfo || tokenInfo.expires_time < Date.now()) {
-    tokenInfo = await getAccessToken();
-  }
-  try {
-    const params = new URLSearchParams({
-      part: 'snippet',
-      access_token: tokenInfo.access_token,
-    });
-    const {
-      data,
-    } = await axios.post(`${liveChatUrl}?${params}`, {
-      snippet: {
-        type: 'textMessageEvent',
-        liveChatId,
-        textMessageDetails: {
-          messageText,
-        },
-      },
-    });
-    return data;
-  } catch (error) {
-    error.data = error.response ? error.response.data : {};
-    throw error;
-  }
-}
-
 module.exports = {
   getAllEvents,
   listenMessages,
-  sendMessage,
 };
